ee702c1f2bb087b14ffcdc314949fb56b91c5a1e
nanibabunalli <nallinani66@gmail.com>
2024-09-25 09:48:39 +0530

    firstcommit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -13,7 +13,9 @@ const locations = [
     reviews: [
       { author: 'Nani Nalli', rating: 5, timestamp: '2013-07-16', reviewText: 'I recently visited Apollo Pharmacy and had a largely positive experience. The store was clean, well-organized, and stocked with a wide range of products, from prescription medications to health supplements. The staff was knowledgeable and helpful, providing detailed information about the medications I needed. Pricing was competitive, with various discounts and a beneficial membership program. While the prescription filling process was smooth and efficient, some items were out of stock, which was a minor inconvenience. Additionally, the online services, including home delivery and prescription uploads, were user-friendly, although tracking could be improved. Overall, Apollo Pharmacy offers reliable service and a pleasant shopping experience, making it a recommended choice for health-related needs.' },
       { author: 'Bala Subhramanyam', rating: 3, timestamp: '2013-06-16', reviewText: 'Convenient location, but the waiting time was a bit long. However, they have a good selection of health products.' }
-    ]
+    ],
+    gmail : 'appolopharamacy56@gmail.com',
+    phno: '9177440337'
   },
   {
     name: 'NetMeds',
@@ -29,7 +31,9 @@ const locations = [
     reviews: [
       { author: '    Shreyansh    ', rating: 4, timestamp: '2013-07-20', reviewText: 'Their online ordering system is easy to use, and delivery is fast. I haven’t had any issues with prescriptions, and their customer service is helpful.'},
       { author: '    Mouli    ', rating: 2, timestamp: '2013-06-25', reviewText: 'Good pharmacy, but sometimes it feels understaffed. The lines can get long during peak hours.' }
-    ]
+    ],
+    gmail : 'netmedspharma@gmail.com',
+    phno: '9177449988'
   },
   {
     name: 'HealthPlus Pharmacy',
@@ -45,7 +49,9 @@ const locations = [
     reviews: [
       { author: '   Srinivas    ', rating: 5, timestamp: '2013-08-10', reviewText: 'A great pharmacy with professional staff. I had some questions about a medication, and they explained everything clearly. Highly recommended.' },
       { author: '   Karthikeya   ', rating: 4, timestamp: '2013-08-15', reviewText: 'Quick service and reasonable prices. I always trust this pharmacy for my familys medical needs' }
-    ]
+    ],
+    gmail : 'healthplus@gmail.com',
+    phno: '91878906798'
   },
   {
     name: 'Wellness Pharmacy',
@@ -61,7 +67,9 @@ const locations = [
     reviews: [
       { author: '      Pranay Reddy     ', rating: 4, timestamp: '2013-09-12', reviewText: 'Nice and clean pharmacy. The staff is very polite, and I found everything I needed for my child’s allergies. Will visit again.' },
       { author: '   Koushik reddy      ', rating: 3, timestamp: '2013-09-18', reviewText: 'I like their wellness section. Lots of organic and natural products that are hard to find elsewhere' }
-    ]
+    ],
+    gmail : 'wellnesspharmacy@gmail.com',
+    phno: '9177499887'
   },
   {
     name: 'Care Pharmacy',
@@ -77,7 +85,9 @@ const locations = [
     reviews: [
       { author: '      Akash     ', rating: 3, timestamp: '2013-10-20', reviewText: 'The pharmacist here is very knowledgeable, and they always make sure I understand my medications before I leave. Great customer service!.' },
       { author: '     Tej         ', rating: 4, timestamp: '2013-10-25', reviewText: 'Good prices on over-the-counter products. Sometimes they run out of stock on popular items, though.' }
-    ]
+    ],
+    gmail : 'carepharma88@gmail.com',
+    phno: '9987654328'
   },
   {
     name: 'MedPlus Pharmacy',
@@ -93,7 +103,9 @@ const locations = [
     reviews: [
       { author: '       Hemanth      ', rating: 4, timestamp: '2013-11-12', reviewText: 'I had a prescription that was hard to fill, but the pharmacist here went the extra mile to find an alternative for me. Super helpful!' },
       { author: '       Abhi         ', rating: 3, timestamp: '2013-11-15', reviewText: 'Conveniently located, but the interior is a bit cramped. They have a good variety of health products, though' }
-    ]
+    ],
+    gmail : 'Medpluspharamacy@gmail.com',
+    phno: '98906754356'
   },
   {
     name: 'LifePharma',
@@ -109,7 +121,9 @@ const locations = [
     reviews: [
       { author: '     Harsha      ', rating: 5, timestamp: '2013-12-02', reviewText: 'Great experience! The pharmacy is modern and well-stocked, and they have a dedicated area for consultations, which I found really helpful.' },
       { author: '       Shannu      ', rating: 4, timestamp: '2013-12-10', reviewText: ' trust this pharmacy for all my health needs. The staff is very professional, and they offer a wide range of services.' }
-    ]
+    ],
+    gmail : 'lifepharmapharamacy6@gmail.com',
+    phno: '9189765437'
   },
   {
     name: 'WellCare Pharmacy',
@@ -125,7 +139,9 @@ const locations = [
     reviews: [
       { author: '      Deepak       ', rating: 4, timestamp: '2014-01-08', reviewText: 'The pharmacy offers great customer care, and they even deliver my medication. The staff is always friendly and polite.' },
       { author: '       Shiva        ', rating: 3, timestamp: '2014-01-12', reviewText: 'Nice place to get basic health essentials. It’s a small pharmacy, but they offer quick service.' }
-    ]
+    ],
+    gmail : 'wellcarepharamacy6@gmail.com',
+    phno: '9909876537'
   },
   {
     name: 'CuraPharm',
@@ -141,7 +157,9 @@ const locations = [
     reviews: [
       { author: '          Akhil       ', rating: 3, timestamp: '2014-02-10', reviewText: 'I’ve been coming here for years. The pharmacists know me by name and always provide excellent advice on my prescriptions.' },
       { author: '       Rishi          ', rating: 4, timestamp: '2014-02-15', reviewText: 'A hidden gem! They carry a wide range of supplements and natural remedies, and the staff is very knowledgeable.' }
-    ]
+    ],
+    gmail : 'curepharma56@gmail.com',
+    phno: '9178990087'
   }
 ]
 
@@ -173,12 +191,13 @@ const locationInfo = (req, res) => {
     title: location.name,
     pageHeader: { title: location.name },
     sidebar: {
-      context: `${location.name} is on Loc8r because it has great services and accessible wifi.`,
-      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.',
+      context: `${location.name} is featured on Pharmacy Locator because it offers excellent healthcare services and customer care.`,
+      callToAction: `To Contact Us:\nGmail: ${location.gmail}\nPhone: ${location.phno}`
     },
     location: location
   });
 };
+
 
 
 //Add Review Page
